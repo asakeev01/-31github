23fcf517d43c5e8eae80748c419f68ed68139291
asakeev01 <aidar.asakeev@gmail.com>
2023-11-09 16:42:48 +0600

    Solved rotatedArray2

--- searchInRotatedArray.py
+++ searchInRotatedArray.py
@@ -1,13 +1,15 @@
 def search(self, nums: List[int], target: int) -> int:
-    min_num = target
     min_ind = 0
+    max_ind = len(nums) - 1
     nums_len = len(nums)
-    for i in range(nums_len):
-        if min_num >= nums[i]:
-            min_num = nums[i]
-            min_ind = i
     left = 0
     right = nums_len - 1
+    while min_ind < max_ind:
+        mid1 = (min_ind + max_ind) // 2
+        if nums[mid1] > nums[max_ind]:
+            min_ind = mid1 + 1
+        else:
+            max_ind = mid1
     while left <= right:
         mid = (right + left) // 2
         mid_pivoted = (mid + min_ind) % nums_len
