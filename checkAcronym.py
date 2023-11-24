12cf9b21365169635ae95a7115a58fa8d180fee3
asakeev01 <aidar.asakeev@gmail.com>
2023-11-24 21:56:03 +0600

    Solved checkAcronym

---
+++ checkAcronym.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def isAcronym(self, words: List[str], s: str) -> bool:
+    if len(words) != len(s):
+        return False
+    else:
+        for i in range(len(words)):
+            if words[i][0] != s[i]:
+                return False
+    return True
