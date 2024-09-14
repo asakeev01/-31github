77c4d43db534bdb844fee0df3d8ecf899a637bce
asakeev01 <aidar.asakeev@gmail.com>
2024-09-13 23:18:17 -0500

    Solved maxFrequencyElements

---
+++ maxFrequencyElements.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        d = {}
+        m = 1
+        counter = 0
+        for i in range(len(nums)):
+            if nums[i] in d:
+                d[nums[i]] += 1
+                if d[nums[i]] > m:
+                    m = d[nums[i]]
+            else:
+                d[nums[i]] = 1
+        for v in d.values():
+            if v == m:
+                counter += v
+        return counter
