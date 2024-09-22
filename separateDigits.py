fa1b9942933d2d6d85e4a0365f48a64cb7f859ff
asakeev01 <aidar.asakeev@gmail.com>
2024-09-21 19:13:32 -0500

    Solved separateDigits

---
+++ separateDigits.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        ans = []
+        for num in nums:
+            for d in str(num):
+                ans.append(int(d))
+        return ans
