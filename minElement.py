c871c899277d9471e3af1687d2fa44d122e14c46
asakeev01 <aidar.asakeev@gmail.com>
2025-02-02 18:34:45 -0600

    Solved minElement

---
+++ minElement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        for i, num in enumerate(nums):
+            elem = 0
+            for digit in str(num):
+                elem += int(digit)
+            nums[i] = elem
+        return min(nums)
