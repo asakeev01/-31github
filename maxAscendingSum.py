7a89258cf4a4268e51e39d47521ffdf8216cfeea
asakeev01 <aidar.asakeev@gmail.com>
2025-02-04 13:31:49 -0600

    Solved maxAscendingSum

---
+++ maxAscendingSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        answer = nums[0]
+        current = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                current += nums[i]
+                if current > answer:
+                    answer = current
+            else:
+                if current > answer:
+                    answer = current
+                current = nums[i]
+
+        return answer
