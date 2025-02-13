aadc959c65dc82f1a8ca89a3a2d2ecf972c8558e
asakeev01 <aidar.asakeev@gmail.com>
2025-02-12 19:13:36 -0600

    Added import

--- minOperationsL.py
+++ minOperationsL.py
@@ -1,13 +1,17 @@
+import heapq
+from typing import List
+
+
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         heapq.heapify(nums)
         counter = 0
         while True:
-            num1 = heappop(nums)
+            num1 = heapq.heappop(nums)
             if num1 >= k:
                 break
-            num2 = heappop(nums)
+            num2 = heapq.heappop(nums)
             num = (num1 * 2) + num2
-            heappush(nums, num)
+            heapq.heappush(nums, num)
             counter += 1
         return counter
