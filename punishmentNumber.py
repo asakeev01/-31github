b00b41e55fb5a357665fe14a209615934b025a81
asakeev01 <aidar.asakeev@gmail.com>
2025-02-15 13:14:43 -0600

    Solved punishmentNumber

---
+++ punishmentNumber.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canPartition(self, s: str, target: int) -> bool:
+        if s == "" and target == 0:
+            return True
+        if target < 0:
+            return False
+        ans = False
+        for i in range(len(s)):
+            left = s[: i + 1]
+            right = s[i + 1 :]
+            leftNum = int(left)
+
+            isPossible = self.canPartition(right, target - leftNum)
+            if isPossible:
+                ans = True
+                break
+        return ans
+    def punishmentNumber(self, n: int) -> int:
+        sum_ = 0
+        for num in range(1, n + 1):
+            sqr = num * num
+            if self.canPartition(str(sqr), num):
+                sum_ += sqr
+        return sum_
