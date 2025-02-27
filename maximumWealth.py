72ecd87bb8b2088ef6b2e0931879f1ce07a22c85
asakeev01 <aidar.asakeev@gmail.com>
2025-02-26 16:25:11 -0800

    Solved maximumWealth

---
+++ maximumWealth.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maxNum = 0
+        for i in range(len(accounts)):
+            temp = 0
+            for j in range(len(accounts[i])):
+                temp += accounts[i][j]
+            if temp > maxNum:
+                maxNum = temp
+        return maxNum
